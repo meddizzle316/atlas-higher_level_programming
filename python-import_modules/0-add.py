4797ec001d2fa511690f329e7daa5d455ecaec77
meddizzle316 <124944809+meddizzle316@users.noreply.github.com>
2024-01-04 16:09:12 -0600

    added main function

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 from add_0.py import add
-a = 1
-b = 2
-print("{} + {} = {}".format(a, b, add(a, b)))
+def main():
+    a = 1
+    b = 2
+    print("{} + {} = {}".format(a, b, add(a, b)))
