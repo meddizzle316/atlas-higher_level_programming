d513345f4af36af04ffb2a1ed964b4250b311a2c
meddizzle316 <124944809+meddizzle316@users.noreply.github.com>
2024-01-04 14:30:11 -0600

    'removed trailing whitespace'

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def uppercase(str):
     str = str + '\n'
-    for i in range(len(str)): 
+    for i in range(len(str)):
         ascii_value = ord(str[i])
         if ascii_value > 96 and ascii_value < 123:
             print("{}".format(chr(ascii_value - 32)), end="")
