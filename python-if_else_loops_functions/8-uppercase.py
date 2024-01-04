fcd1a553ab84dc9488ea97172e9b5b6274bfeb99
meddizzle316 <meddizzle316@gmail.com>
2024-01-04 09:36:00 -0600

    added permissions and removed trailing whitespace from 8

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def uppercase(str):
-    for i in range(len(str)): 
+    for i in range(len(str)):
         if ord(str[i]) > 96 and ord(str[i] < 123):
             print("{}".format(ord(str[i] - 32)))
         else:
