9fbf7f5e5dc8c2135102b2418e4287d91367a363
meddizzle316 <124944809+meddizzle316@users.noreply.github.com>
2024-01-04 09:05:46 -0600

    'think I have something'

---
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+for i in range(10):
+    for x in range(10):
+        if not i == x and x > i and i < 8:
+            print("{}{}".format(i, x), end=", ")
+        elif not i == x and x > i and i == 8:
+            print("{}{}".format(i, x))
+        
