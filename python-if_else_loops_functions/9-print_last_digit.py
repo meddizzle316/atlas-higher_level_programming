545cc68eb67f0192310c8c487221d15a4141e72d
meddizzle316 <124944809+meddizzle316@users.noreply.github.com>
2024-01-04 14:35:09 -0600

    'removed newline'

--- python-if_else_loops_functions/9-print_last_digit.py
+++ python-if_else_loops_functions/9-print_last_digit.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def print_last_digit(number):
     if number >= 0:
-        print (number % 10)
+        print (number % 10, end="")
     elif number < 0:
-        print ((abs(number) % 10) * -1)
+        print ((abs(number) % 10) * -1, end="")
