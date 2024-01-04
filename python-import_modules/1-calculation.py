ac1cca97669188f5c222e732d850558ba268ea46
meddizzle316 <124944809+meddizzle316@users.noreply.github.com>
2024-01-04 16:23:19 -0600

    calculator 1 only mentioend once

--- python-import_modules/1-calculation.py
+++ python-import_modules/1-calculation.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
-from calculator_1 import add
-from calculator_1 import sub
-from calculator_1 import div
-from calculator_1 import mul
+from calculator_1 import add, sub, div, mul
+
 
 def main():
     a = 10
