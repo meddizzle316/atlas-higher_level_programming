76e3b36a5e522716507b6675015ff6b2fe943219
meddizzle316 <124944809+meddizzle316@users.noreply.github.com>
2024-01-04 14:36:47 -0600

    'now returns the number also'

--- python-if_else_loops_functions/9-print_last_digit.py
+++ python-if_else_loops_functions/9-print_last_digit.py
@@ -2,5 +2,7 @@
 def print_last_digit(number):
     if number >= 0:
         print (number % 10, end="")
+        return (number % 10)
     elif number < 0:
         print ((abs(number) % 10) * -1, end="")
+        return((abs(number) % 10) * -1)
