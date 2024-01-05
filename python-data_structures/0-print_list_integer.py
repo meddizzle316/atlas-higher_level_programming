a2dc8fe636534ecba0e4aa5db357b7840ba9d96e
meddizzle316 <124944809+meddizzle316@users.noreply.github.com>
2024-01-05 10:59:03 -0600

    asdf

--- python-data_structures/0-print_list_integer.py
+++ python-data_structures/0-print_list_integer.py
@@ -2,4 +2,4 @@
 def print_list_integer(my_list=[]):
     if len(my_list) > 0:
         placeholders = '\n'.join(['{:d}'] * len(my_list)) 
-    print(placeholders.format(*my_list))
+        print(placeholders.format(*my_list))
