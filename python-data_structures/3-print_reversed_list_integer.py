0f16780a1da03b41e754781c996f1ef2cf8f57f7
meddizzle316 <124944809+meddizzle316@users.noreply.github.com>
2024-01-05 13:31:01 -0600

    none case

--- python-data_structures/3-print_reversed_list_integer.py
+++ python-data_structures/3-print_reversed_list_integer.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python3
+import sys
+
+
 def print_reversed_list_integer(my_list=[]):
+    if my_list == None:
+        sys.exit()    
     if len(my_list) > 0:
         placeholders = '\n'.join(['{:d}'] * len(my_list))
         print(placeholders.format(*my_list[::-1]))
