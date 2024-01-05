d1ea85e45d2bbbd1da3d8b1e6b83a4bdbe1082c3
meddizzle316 <124944809+meddizzle316@users.noreply.github.com>
2024-01-04 20:36:34 -0600

    first go at 3

---
+++ python-import_modules/3-infinite_add.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+import sys
+
+
+def main(args):
+    result = 0
+    if args == 0:
+        print("0")
+        sys.exit()
+    for i in range(len(args)):
+        result += int(args[i])
+    return result
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
