dc898d99d8a734648a9e142a661de739c531dea2
meddizzle316 <124944809+meddizzle316@users.noreply.github.com>
2024-01-06 13:50:05 -0600

    from opython tutor

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    new_string = my_string[:]
+    new_string = ''
     if my_string is None:
         pass
-    elif len(my_string) > 0:
-        new_string = my_string.replace("c","")
-        new_string = my_string.replace("C","")
-        return new_string
+    for i in range(len(my_string)):
+        if my_string[i] == 'c' or my_string[i] == 'C':
+            new_string += "" 
+        else:
+            new_string = new_string + my_string[i]
+    return new_string
