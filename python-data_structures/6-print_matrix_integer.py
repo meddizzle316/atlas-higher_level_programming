e73a0222247683f3eb259550a11d7c45a23dfa7a
meddizzle316 <124944809+meddizzle316@users.noreply.github.com>
2024-01-06 15:07:52 -0600

    asdf

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
 def print_matrix_integer(m=[[]]):
-    print ('\n'.join([' '.join(['{:d}'.format(i) for i in r]) for r in m]))
+    print('\n'.join([' '.join(['{:d}'.format(i) for i in r]) for r in m]))
