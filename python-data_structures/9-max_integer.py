6d24f46dc1f995d75b94e5e21cfb4c216f112f22
meddizzle316 <124944809+meddizzle316@users.noreply.github.com>
2024-01-07 15:06:05 -0600

    added int min to max initial value

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    max = 0
+    max = -9223372036854775
     if my_list is None or len(my_list) == 0:
         return None
     for i in range(len(my_list)):
