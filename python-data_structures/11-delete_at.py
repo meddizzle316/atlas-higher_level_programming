43068f6077fb11a23aaa440b2718b1b751176d1a
meddizzle316 <124944809+meddizzle316@users.noreply.github.com>
2024-01-07 18:26:00 -0600

    changed loop end condition

--- python-data_structures/11-delete_at.py
+++ python-data_structures/11-delete_at.py
@@ -2,7 +2,7 @@
 def delete_at(my_list=[], idx=0):
     if idx < 0 or idx > len(my_list):
         return my_list
-    for i in range(len(my_list)):
+    for i in range(len(my_list) + 1):
         if i == idx + 1:
             my_list.remove(i)
     return my_list
