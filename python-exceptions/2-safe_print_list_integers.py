6967a7d20f5f8d61006ce461a9093750d6c6b7e2
meddizzle316 <124944809+meddizzle316@users.noreply.github.com>
2024-01-09 17:01:27 -0600

    add 2

---
+++ python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+    result = 0 
+    try:
+        for i in range(x):
+            if type(my_list[i]) is type(i):
+                print("{:d}".format(my_list[i]), end="")
+                result += 1
+        print()
+    except IndexError:
+        print()
+        return (result)
+    else:
+        return (result)
