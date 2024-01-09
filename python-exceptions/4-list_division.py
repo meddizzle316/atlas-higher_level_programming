614b6496d7bcead2289fbc88fca096505efe2439
meddizzle316 <124944809+meddizzle316@users.noreply.github.com>
2024-01-09 17:31:05 -0600

    add 4

---
+++ python-exceptions/4-list_division.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+def list_division(my_list_1, my_list_2, list_length):
+    result = 0
+    i = 0
+    new_list = []
+    while i < list_length:
+        try:
+            new_list.append(int(my_list_1[i]) / int(my_list_2[i]))
+        except ZeroDivisionError:
+            print("division by 0")
+            new_list.append(0)
+        except IndexError:
+            print("out of range")
+            new_list.append(0)
+        except (TypeError, ValueError):
+            print("wrong type")
+            new_list.append(0)
+        finally:
+            i += 1
+    return new_list
