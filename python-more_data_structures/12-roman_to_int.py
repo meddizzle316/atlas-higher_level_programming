bb9f67f3259efdaf7a18d0e07ab3cadd6fa0280e
meddizzle316 <124944809+meddizzle316@users.noreply.github.com>
2024-01-09 14:34:32 -0600

    add first version of 12

---
+++ python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    roman_to_int = {'I': 1, 'V':5, 'X': 10, 'L': 50,
+    'C': 100, 'D': 500, 'M': 1000}
+    result = 0
+    for i in range(len(roman_string)):
+        if roman_string[i] in roman_to_int:
+            result += roman_to_int[roman_string[i]]
+    return result
