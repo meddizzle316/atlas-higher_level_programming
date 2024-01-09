bb0ef45bc76755016fae9fd3d29873043c03796c
meddizzle316 <124944809+meddizzle316@users.noreply.github.com>
2024-01-09 17:16:18 -0600

    add 3

---
+++ python-exceptions/3-safe_print_division.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def safe_print_division(a, b):
+    result = 0
+    try:
+        result = a / b
+    except ZeroDivisionError:
+        result = None
+    finally:
+        if result is not None:
+            print("Inside result: {:.1f}".format(result))
+        else:
+            print("Inside result: {}".format(None))
+        return result
