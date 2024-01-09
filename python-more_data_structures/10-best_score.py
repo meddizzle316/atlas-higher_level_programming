7ab9614badfffd044f2ac8dba3f19278b346de59
meddizzle316 <124944809+meddizzle316@users.noreply.github.com>
2024-01-09 11:15:35 -0600

    added 10

---
+++ python-more_data_structures/10-best_score.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def best_score(a_dictionary):
+    if a_dictionary is None:
+        return None
+    values_list = list(a_dictionary.values())
+    max = -922337203685477
+    for i in range(len(values_list)):
+        if values_list[i] > max:
+            max = values_list[i]
+    if max == -922337203685477:
+        return None
+    return max
