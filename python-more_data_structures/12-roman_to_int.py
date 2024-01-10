edd8eae11dee67b326d07790d4f6f2665c96eb33
meddizzle316 <124944809+meddizzle316@users.noreply.github.com>
2024-01-09 19:55:38 -0500

    asdf

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -3,7 +3,7 @@ def roman_to_int(roman_string):
     dict = {'I': 1, 'V':5, 'X': 10, 'L': 50,
     'C': 100, 'D': 500, 'M': 1000}
     result = 0
-    if roman_string is None or isinstance(roman_string, str) == False:
+    if roman_string is None or not isinstance(roman_string, str):
         return None
     for i in range(len(roman_string)):
         s = roman_string[i]
