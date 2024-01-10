67cb341ad34b9ebed451e969d3627adc1c5ce679
meddizzle316 <124944809+meddizzle316@users.noreply.github.com>
2024-01-09 19:15:48 -0500

    added isinstance to check value

--- python-exceptions/1-safe_print_integer.py
+++ python-exceptions/1-safe_print_integer.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 def safe_print_integer(value):
     try:
-        int_value = int(value)
-        print("{:d}".format(int_value))
-        return True    
+        if (isinstance(value, int)):
+            int_value = int(value)
+            print("{:d}".format(int_value))
+            return True    
     except Exception:
         return False
