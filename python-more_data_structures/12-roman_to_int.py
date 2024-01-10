c6b76cb13284f60f4df5c3a156ef063e5b8b4075
meddizzle316 <124944809+meddizzle316@users.noreply.github.com>
2024-01-09 19:53:48 -0500

    added null case

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -3,6 +3,8 @@ def roman_to_int(roman_string):
     dict = {'I': 1, 'V':5, 'X': 10, 'L': 50,
     'C': 100, 'D': 500, 'M': 1000}
     result = 0
+    if roman_string is None or isinstance(roman_string, str) == False:
+        return None
     for i in range(len(roman_string)):
         s = roman_string[i]
         try:
