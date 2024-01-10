47107bc13bec1ef45c410e15d7fefc4393dd81e8
meddizzle316 <124944809+meddizzle316@users.noreply.github.com>
2024-01-09 20:14:55 -0500

    asdf

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -1,14 +1,13 @@
 #!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
     result = 0 
-    try:
-        for i in range(x):
+    for i in range(x):
+        try:
             if type(my_list[i]) is type(i):
                 print("{:d}".format(my_list[i]), end="")
                 result += 1
-        print()
-    except Exception:
-        print()
-        return (result)
-    else:
-        return (result)
+                print()
+        except Exception:
+            continue
+        else:
+            return (result)
