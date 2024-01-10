5c1bd1287c3e53ec9abeb0e2090a5b72b88998cc
meddizzle316 <124944809+meddizzle316@users.noreply.github.com>
2024-01-09 19:42:53 -0500

    negative numbers to 12

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,9 +1,17 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    roman_to_int = {'I': 1, 'V':5, 'X': 10, 'L': 50,
+    dict = {'I': 1, 'V':5, 'X': 10, 'L': 50,
     'C': 100, 'D': 500, 'M': 1000}
     result = 0
     for i in range(len(roman_string)):
-        if roman_string[i] in roman_to_int:
-            result += roman_to_int[roman_string[i]]
+        s = roman_string[i]
+        try:
+            n = roman_string[i + 1]
+            if s in dict and n in dict and dict[s] < dict[n]:
+                result -= dict[s]
+            elif s in dict:
+                result += dict[s]
+        except IndexError:
+            if s in dict:
+                result += dict[s]
     return result
