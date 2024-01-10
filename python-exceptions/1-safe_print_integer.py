dd2957635919781daa182feadded813c27a31ce2
meddizzle316 <124944809+meddizzle316@users.noreply.github.com>
2024-01-09 19:12:40 -0500

    something for 1

--- python-exceptions/1-safe_print_integer.py
+++ python-exceptions/1-safe_print_integer.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 def safe_print_integer(value):
     try:
-        print("{:d}".format(int(value)))
+        int_value = int(value)
+        print("{:d}".format(int_value))
         return True    
     except Exception:
         return False
