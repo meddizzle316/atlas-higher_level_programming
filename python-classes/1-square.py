198bebb66bce27c4b28079092c1bda2ea469e98b
meddizzle316 <124944809+meddizzle316@users.noreply.github.com>
2024-01-11 10:55:07 -0600

    add 1

---
+++ python-classes/1-square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""This is a basic training module designed to teach us how to use classes
+
+"""
+##asldkfjasdlkf
+
+class Square:
+    """This is a class for a square
+
+    We have a basic init function
+
+    Note:
+        Do not include the self parameter
+
+    Args:
+       size (int): The first parameter
+    """
+    def __init__(self, size):
+        self._size = size
