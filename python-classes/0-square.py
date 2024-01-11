33ee828ee13b27daccdc7754a7337b508e719805
meddizzle316 <124944809+meddizzle316@users.noreply.github.com>
2024-01-11 10:44:32 -0600

    asdf

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,6 +1,12 @@
 #!/usr/bin/python3
 
 class Square: 
+    """This is a class for a square
+    
+    We have a basic init function
 
+    Note: 
+        Do not include the self parameter
+    """
     def __init__(self):
         pass
