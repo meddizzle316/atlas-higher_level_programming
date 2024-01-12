406820bc118e75313cafdeecb467ce74bb04ff4f
meddizzle316 <124944809+meddizzle316@users.noreply.github.com>
2024-01-12 11:18:26 -0600

    add 0

---
+++ python-test_driven_development/0-add_integer.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""This is a basic module to help us learn about unit tests
+
+"""
+
+
+def add_integer(a, b=98):
+    if not isinstance(a, (int, float)):
+        raise TypeError("a must be an integer")
+    if not isinstance(b, (int, float)):
+        raise TypeError("b must be an integer")
+    if isinstance(a, float):
+        a = int(a)
+    if isinstance(b, float):
+        b = int(b)
+    return a + b
+    
