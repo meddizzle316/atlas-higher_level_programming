e963137f015464c040b94864b42317f60fd75150
meddizzle316 <124944809+meddizzle316@users.noreply.github.com>
2024-01-15 10:20:01 -0600

    asdf

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -14,8 +14,3 @@ def add_integer(a, b=98):
     if isinstance(b, float):
         b = int(b)
     return a + b
-    
-
-if __name__ == "__main__":
-    import doctest
-    doctest.testfile('0-add_integer.txt')
