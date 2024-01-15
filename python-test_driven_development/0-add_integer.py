63d0958a32547a4766d72ff876c72f48e8dc1a34
meddizzle316 <124944809+meddizzle316@users.noreply.github.com>
2024-01-15 11:26:00 -0600

    asdf

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -5,6 +5,9 @@
 
 
 def add_integer(a, b=98):
+    """
+    Function to add integers
+    """
     if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
     if not isinstance(b, (int, float)):
