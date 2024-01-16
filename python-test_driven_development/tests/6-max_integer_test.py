5565d2830e29d297f1b156dc04affe9f5288f63c
meddizzle316 <124944809+meddizzle316@users.noreply.github.com>
2024-01-16 11:24:50 -0600

    asdf

--- python-test_driven_development/tests/6-max_integer_test.py
+++ python-test_driven_development/tests/6-max_integer_test.py
@@ -4,37 +4,11 @@ import unittest
 """
 
 
-def max_integer(list=[]):
-    """Doc
-    """
-    if len(list) == 0:
-        return None
-    result = list[0]
-    i = 1
-    while i < len(list):
-        if list[i] > result:
-            result = list[i]
-        i += 1
-
-    # If middle element => find another integer
-    if list[0] != result and list[-1] != result and len(list) > 2:
-        old_result = result
-        result = list[0]
-        i = 1
-        while i < len(list):
-            if list[i] != old_result:
-                result = list[i]
-                break
-            i += 1
-   
-    return result
-
 class TestMaxInt(unittest.TestCase):
     
     def test_max_at_end(self):
         list = [1, 2, 3, 4]
-        result = max_integer(list)
-        self.assertEqual(result, 4)
+        self.assertEqual(max_integer(list), 4)
     def test_same_value(self):
         list = [1, 1, 1, 1]
         result = max_integer(list)
