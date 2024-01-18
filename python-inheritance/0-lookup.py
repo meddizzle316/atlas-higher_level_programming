b981839ed30be546c08a6e01d86bb0f2be72bddc
meddizzle316 <124944809+meddizzle316@users.noreply.github.com>
2024-01-18 17:13:11 -0600

    add doc

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
 """
 Module for learning inheritance
+has a lookup function
 """
+
+
 def lookup(obj):
     """ function that returns a list dictionary"""
     return list(obj.__dict__)
