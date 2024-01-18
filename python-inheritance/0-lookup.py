07bfb8960f5d2eeb4510672dd690a04d29ced5a0
meddizzle316 <124944809+meddizzle316@users.noreply.github.com>
2024-01-18 17:20:35 -0600

    asdf

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -2,9 +2,13 @@
 """
 Module learning inheritance
 has a lookup function
+
+Lookup:
+    arg: obj
+    return: new list
 """
 
 
 def lookup(obj):
-    """ function that returns a list dictionary"""
+    """ function that returns a list dictionary """
     return list(obj.__dict__)
