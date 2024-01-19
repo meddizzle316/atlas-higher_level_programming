b8185b02ce7f871b8ac0561842a62b3d316f23a3
meddizzle316 <124944809+meddizzle316@users.noreply.github.com>
2024-01-19 13:03:25 -0600

    asdf

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -3,5 +3,5 @@
 
 
 def write_file(filename="", text=""):
-    with open(filename, 'w', encoding='utf-8'):
-        return (filename.write(text))
+    with open(filename, 'w', encoding='utf-8') as file:
+        return (file.write(text))
