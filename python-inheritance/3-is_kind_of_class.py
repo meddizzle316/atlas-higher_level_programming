ba1488715e792b088fc18e17be572808d568e9d1
meddizzle316 <124944809+meddizzle316@users.noreply.github.com>
2024-01-18 18:34:54 -0600

    added

---
+++ python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+""" a module for is_kind of class"""
+
+def is_kind_of_class(obj, a_class):
+    """ returns true if obj is instance a_class or inherit"""
+    return isinstance(obj, a_class)
