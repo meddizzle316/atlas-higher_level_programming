e5aeed3ccefc2670772d3d9b03ea0346fdd91781
meddizzle316 <124944809+meddizzle316@users.noreply.github.com>
2024-01-19 17:13:28 -0600

    asdf

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,8 +1,13 @@
 #!/usr/bin/python3
-"module about reading files in python"
+"""
+module about reading files in python
+It has a single function 
+called read_file 
+"""
 
 
 def read_file(filename=""):
+    """reads from a given file and prints"""
     with open(filename, 'r') as file:
         df = file.read()
         print(df, end="")
