8dfa2798db35b99bd0aa5ae0d7c510df5eb7c256
meddizzle316 <124944809+meddizzle316@users.noreply.github.com>
2024-01-18 18:03:41 -0600

    asdf

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
-" A module to learn inheritanc"
+""" A module to learn inheritance """
 
 
 class MyList(list):
-    " a custom class for a list"
+    """ a custom class for a list """
     def print_sorted(self):
-        " prints a sorted list, no return"
+        """ prints a sorted list, no return """
         sorted_list = self[:]
         sorted_list.sort()
         print(sorted_list)
