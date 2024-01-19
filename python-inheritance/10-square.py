5eb7a2ac135eb9b2fb58f6023c38fd93229fa7fb
meddizzle316 <124944809+meddizzle316@users.noreply.github.com>
2024-01-19 17:07:38 -0600

    asdf

--- python-inheritance/10-square.py
+++ python-inheritance/10-square.py
@@ -1,18 +1,27 @@
 #!/usr/bin/python3
 """
 Module learning inheritance
-has a lookup function
+has a square class
 
-Lookup:
-    arg: obj
-    return: new list
+Square class:
+    attributes:
+        __size: private variable 
+        of square
+
+    methods:
+        __init__-- initializes function
+        validates size 
+        area: returns area
+        __str__: customizes string 
+        representation
 """
-
 
 Rectangle = __import__('9-rectangle.py').Rectangle
 
 class Square(Rectangle):
     """a module for square class, inherits from rectangle"""
+
+
     def __init__(self, size):
         super().integer_validator(size)
         self.__size = size
