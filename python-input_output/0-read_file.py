07e809cff2a723afe6bdc3cf474377438582fc85
meddizzle316 <124944809+meddizzle316@users.noreply.github.com>
2024-01-19 11:06:53 -0600

    add 0

---
+++ python-input_output/0-read_file.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""a module about reading files in python"""
+
+def read_file(filename=""):
+    with open('filename', 'r') as file:
+        df = file.read()
+        print(df)
+        
+
