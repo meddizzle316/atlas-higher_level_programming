67bbb908bdb979514807643969446d4f72b8012b
meddizzle316 <124944809+meddizzle316@users.noreply.github.com>
2024-01-19 13:12:03 -0600

    add 2

---
+++ python-input_output/2-append_write.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""A module for task 2; appending text"""
+
+
+def append_write(filename="", text=""):
+    with open(filename, 'a', encoding='utf-8') as file:
+        return (file.write(text))
