68d2cbc74fce90b0f61597f272341a718100c803
meddizzle316 <124944809+meddizzle316@users.noreply.github.com>
2024-01-19 13:15:07 -0600

    asdf

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,9 +1,21 @@
 #!/usr/bin/python3
-""" A module to learn inheritance """
+"""
+Module for a class 'MyList' that inherits from 'list'
+"""
 
 
 class MyList(list):
-    """ a custom class for a list """
+    """
+    Class for sorting a list
+
+    Args:
+        list: Used for sorting in ascending order
+
+    Attributes: N/A
+
+    Methods:
+        print_sorted: Prints the list in ascending order
+    """
     
     def print_sorted(self):
         """ prints a sorted list, no return """
