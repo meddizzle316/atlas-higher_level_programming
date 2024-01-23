a1ad6d63db156475e731b0432bfa5c5a866d830c
meddizzle316 <124944809+meddizzle316@users.noreply.github.com>
2024-01-23 10:35:32 -0600

    asdf

--- python-input_output/3-to_json_string.py
+++ python-input_output/3-to_json_string.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
-import json
 """a module for task 3"""
+import json
+
 
 def to_json_string(my_obj):
+    """returns a Json representation of my_obj"""
     return (json.dumps(my_obj))
