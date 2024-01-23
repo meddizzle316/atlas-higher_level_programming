8b220222a64c6598b8e1e19f0ff57c55b1fe3352
meddizzle316 <124944809+meddizzle316@users.noreply.github.com>
2024-01-23 10:17:19 -0600

    asdf

--- python-input_output/2-append_write.py
+++ python-input_output/2-append_write.py
@@ -3,5 +3,6 @@
 
 
 def append_write(filename="", text=""):
+    """function that appends text to a file"""
     with open(filename, 'a', encoding='utf-8') as file:
         return (file.write(text))
