500d0e94c5360c8481807cee0938a37406072782
meddizzle316 <124944809+meddizzle316@users.noreply.github.com>
2024-01-23 10:45:14 -0600

    asdf

---
+++ python-input_output/5-save_to_json_file.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+writes an object to a text file
+using json respresentation
+"""
+import json
+
+
+def save_to_json_file(my_obj, filename):
+    with open(filename, 'w') as file:
+        json.dump(my_obj, file)
