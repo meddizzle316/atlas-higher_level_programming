230c502668ade3d6a1c50213e821a585e3488bcc
meddizzle316 <124944809+meddizzle316@users.noreply.github.com>
2024-01-23 15:50:48 -0600

    asdf

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -5,7 +5,10 @@ Python list, and then save them
 to a file
 """
 import json
+import sys
 
 
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+arg = sys.arg[1:]
+print(arg)
