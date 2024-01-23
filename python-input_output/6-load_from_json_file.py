c96df99b2787dbf02334947d44f2cab20a468834
meddizzle316 <124944809+meddizzle316@users.noreply.github.com>
2024-01-23 12:39:11 -0600

    asdf

---
+++ python-input_output/6-load_from_json_file.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+A module with a function 
+to create a object from json
+"""
+import json
+
+
+def load_from_json_file(filename):
+    """returns Py Object from Json"""
+    with open(filename, 'w') as file:
+        return json.load(file)
