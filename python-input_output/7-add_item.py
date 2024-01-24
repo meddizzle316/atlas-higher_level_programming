38fa248ba7c0488837e32d730bb113fe83f994ab
meddizzle316 <124944809+meddizzle316@users.noreply.github.com>
2024-01-23 21:40:42 -0600

    asdf

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -11,7 +11,8 @@ import sys
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 arg = sys.argv[1:]
-for i in arg:
-    save_to_json_file(i, 'add_items.json')
-    print(i)
-load_from_json_file('add_items.json')
+with open('add_items.json', 'w') as file:
+    for i in arg:
+        save_to_json_file(i, file)
+        print(i)
+    load_from_json_file(file)
