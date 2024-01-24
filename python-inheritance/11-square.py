c23d72ed7e82230bd7fb161a4ad6030253e30572
meddizzle316 <124944809+meddizzle316@users.noreply.github.com>
2024-01-23 23:48:19 -0600

    asdf

--- python-inheritance/11-square.py
+++ python-inheritance/11-square.py
@@ -18,10 +18,8 @@ class Square(Rectangle):
     """
     def __init__(self, size):
         super().integer_validator(size)
+        super().__init__(size, size)
         self.__size = size
-    
-    def area(self):
-        return self.__size * self.__size
         
     def __str__(self):
         return (f"[Square] {self.__size}/{self.__size}")
