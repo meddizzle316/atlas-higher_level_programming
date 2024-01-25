3296c4532339383d854fb11749740c26feba0224
meddizzle316 <124944809+meddizzle316@users.noreply.github.com>
2024-01-25 10:25:27 -0600

    asdf

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -21,7 +21,8 @@ class Base():
         objects is incremented
         and id becomes nb_objects
         """
-        if not id is None:
+
+        if id is not None:
             self.id = id
         else:
             Base.__nb_objects += 1
