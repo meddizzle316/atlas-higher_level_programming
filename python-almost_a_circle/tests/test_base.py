dc6e6371bbc3283dec626e05d1d3e2c37c357fe7
meddizzle316 <meddizzle316@gmail.com>
2024-01-26 09:51:49 -0600

    asdf

---
+++ python-almost_a_circle/tests/test_base.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+has tests for base class
+"""
+import unittest
+from models.base import Base
+
+class Test(unittest.TestCase):
+    """Class for unittests on Base class"""
+    def test_expected_id(self):
+        base = Base()
+        self.assertEqual(base.id, 1)
