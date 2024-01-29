2455e067f283f1c37948ce885bfc5e0c29023155
meddizzle316 <meddizzle316@gmail.com>
2024-01-29 13:12:48 -0600

    py fix

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -11,7 +11,7 @@ class Square(Rectangle):
 
     def __str__(self):
         return f"[Square] ({self.id}) {self.x}/{self.y} - {self.width}"
-    
+
     @property
     def size(self):
         return self.width
