41f25c093572ae5d3252e5b07ab913dafb362168
meddizzle316 <meddizzle316@gmail.com>
2024-01-29 13:42:49 -0600

    add none changes

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -31,5 +31,5 @@ class Base():
     @staticmethod
     def to_json_string(list_dictionaries):
         if not any(list_dictionaries):
-            return "[]"
+            return "\"[]\""
         return json.dumps(list_dictionaries)
