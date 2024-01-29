be1b0839f4a6623f89ba2d75026918deaa383192
meddizzle316 <meddizzle316@gmail.com>
2024-01-29 13:41:42 -0600

    add 15 changes

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """base module for almost a circle"""
+import json
 
 
 class Base():
@@ -26,3 +27,9 @@ class Base():
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        if not any(list_dictionaries):
+            return "[]"
+        return json.dumps(list_dictionaries)
