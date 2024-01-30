37769a8cdd88df6eeb578e9fd9cfcbf96fd87c5a
meddizzle316 <meddizzle316@gmail.com>
2024-01-30 10:33:24 -0600

    add 16

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -30,8 +30,22 @@ class Base():
 
     @staticmethod
     def to_json_string(list_dictionaries):
+        """function that converts python lists to json"""
         if not list_dictionaries:
             return "[]"
         if not any(list_dictionaries):
             return "[]"
         return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """
+        converts lists of objects to dictionaries
+        and saves as json string to file
+        """
+        if not list_objs:
+            list_objs = []
+        with open("Rectangle.json", 'w') as file:
+            for i in range(len(list_objs)):
+                new_string = cls.to_json_string((list_objs[i].to_dictionary()))
+                file.writelines(new_string)
