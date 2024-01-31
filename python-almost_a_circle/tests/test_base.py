393af1699f2c56b2b60bac31a553f4b66a4c347a
meddizzle316 <meddizzle316@gmail.com>
2024-01-30 20:36:03 -0600

    adde to base class

--- python-almost_a_circle/tests/test_base.py
+++ python-almost_a_circle/tests/test_base.py
@@ -3,9 +3,12 @@
 has tests for base class
 """
 import unittest
+import os
 from models.base import Base
+from models.square import Square
+from models.rectangle import Rectangle
 
-class Test(unittest.TestCase):
+class BaseTest(unittest.TestCase):
     """Class for unittests on Base class"""
     def test_base_auto_assign_id(self):
         base = Base()
@@ -13,4 +16,11 @@ class Test(unittest.TestCase):
     def test_base_increment_id(self):
         base1 = Base()
         base2 = Base()
-        self.assertEqual(base2.id, 2)
+        self.assertEqual(base2.id, base1.id + 1)
+    def test_signed_id(self):
+        base = Base(89)
+        self.assertEqual(base.id, 89)
+    def test_json_string_none_exists(self):
+        base = Base()
+        Base.to_json_string(None)
+
