7086b94b78dd90f9a23058aa3860fe4cc4e39b5c
meddizzle316 <meddizzle316@gmail.com>
2024-01-30 20:28:36 -0600

    trying a new test

--- python-almost_a_circle/tests/test_base.py
+++ python-almost_a_circle/tests/test_base.py
@@ -10,4 +10,7 @@ class Test(unittest.TestCase):
     def test_base_auto_assign_id(self):
         base = Base()
         self.assertEqual(base.id, 1)
-    
+    def test_base_increment_id(self):
+        base1 = Base()
+        base2 = Base()
+        self.assertEqual(base2.id, 2)
