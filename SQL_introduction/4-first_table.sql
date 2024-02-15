fefb242b2456a4930bcdaed2a05a818941a5ea6b
meddizzle316 <meddizzle316@gmail.com>
2024-02-15 11:23:37 -0600

    add 4

--- SQL_introduction/4-first_table.sql
+++ SQL_introduction/4-first_table.sql
@@ -5,3 +5,7 @@ CREATE TABLE IF NOT EXISTS first_table(
    id INT PRIMARY KEY,
    name VARCHAR(256)
 );
+-- should it be CREATE TABLE IF NOT EXISTS first_table?
+-- ALAS checker already has 100% so it's not running checks
+-- I suppose I could run locally except there's a bunch of stuff
+-- before this??
