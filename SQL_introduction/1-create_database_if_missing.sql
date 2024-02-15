5ac0fa4eb0b007aa0970685b074ec3c17245c730
meddizzle316 <meddizzle316@gmail.com>
2024-02-15 10:58:32 -0600

    add 1

--- SQL_introduction/1-create_database_if_missing.sql
+++ SQL_introduction/1-create_database_if_missing.sql
@@ -2,7 +2,7 @@
 -- if none exists
 -- IF NOT EXISTS ( name FROM master.dbo.sysdatabases WHERE name = N'hbtn_0c_0')
 
-IF DB_ID(['hbtn_0c_0_ID']) IS NOT NULL
+IF DB_ID('hbtn_0c_0_ID') IS NOT NULL
 BEGIN
     CREATE DATABASE hbtn_0c_0;
 END
