73fe9103fdb133590cb9d1eb2489bee2d55fc278
meddizzle316 <meddizzle316@gmail.com>
2024-02-15 10:40:42 -0600

    ADD 1

---
+++ SQL_introduction/1-create_database_if_missing.sql
@@ -0,0 +1,7 @@
+-- an sql script that creates a database
+-- if none exists
+
+IF NOT EXISTS (SELECT name from master.dbo.sysdatabases WHERE name = N'hbtn_0c_0')
+BEGIN
+    CREATE DATABASE hbtn_0c_0;
+END
