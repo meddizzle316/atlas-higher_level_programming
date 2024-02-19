e0d069fa3771a02ec16ab27093973b63b5e49359
meddizzle316 <meddizzle316@gmail.com>
2024-02-19 11:35:52 -0600

    add 13

--- SQL_introduction/12-no_cheating.sql
+++ SQL_introduction/12-no_cheating.sql
@@ -1,7 +1,6 @@
 -- Write a script that updates the 
 -- score of Bob to 10 in the table second_table.
 
--- SELECT * FROM second_table WHERE name = "Bob"
 UPDATE second_table
 set score = 10
 WHERE name = 'Bob'
