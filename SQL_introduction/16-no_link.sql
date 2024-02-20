6cdb8aaf4c88d6b8fe9555400245ac0a1df2d6ec
meddizzle316 <meddizzle316@gmail.com>
2024-02-20 10:36:24 -0600

    add 16

--- SQL_introduction/16-no_link.sql
+++ SQL_introduction/16-no_link.sql
@@ -1,6 +1,6 @@
 -- Write a script that lists all records of the table second_table of 
 -- the database hbtn_0c_0 in your MySQL server.
 
-SELECT * FROM second_table
+SELECT * FROM score, name
 WHERE name IS NOT NULL AND name != ''
 ORDER BY score;
