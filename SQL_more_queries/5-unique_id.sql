7abe072c95b32b2f32b971d9da64529dd0017394
meddizzle316 <meddizzle316@gmail.com>
2024-02-20 12:14:44 -0600

    ADD 5

--- SQL_more_queries/5-unique_id.sql
+++ SQL_more_queries/5-unique_id.sql
@@ -1,6 +1,6 @@
 -- Write a script that creates the table unique_id on your MySQL server.
 
 CREATE TABLE IF NOT EXISTS unique_id (
-    id INT IDENTITY(1,1),
+    id INT DEFAULT 1 UNIQUE,
     name VARCHAR(256)
 );
