06d52eda8e45f9f8bc55114be330c362d356ed5b
meddizzle316 <meddizzle316@gmail.com>
2024-03-23 12:42:24 -0500

    semi fix

--- javascript-warm_up/3-value_argument.js
+++ javascript-warm_up/3-value_argument.js
@@ -2,11 +2,10 @@
 
 for (let i = 0; i <= 2; i++) {
   if (i === 2) {
-      if (process.argv[i] === undefined) {
-        console.log('No argument');
-      }
-      else {
-        console.log(process.argv[i])
-      }
+    if (process.argv[i] === undefined) {
+      console.log('No argument');
+    } else {
+      console.log(process.argv[i]);
+    }
   }
 }
