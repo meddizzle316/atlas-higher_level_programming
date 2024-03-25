1d2e4fcc3f70f0a18c788c91f01b1da5dc68ea0c
meddizzle316 <meddizzle316@gmail.com>
2024-03-25 13:22:53 -0500

    actually it's second biggest not biggest

--- javascript-warm_up/11-second_biggest.js
+++ javascript-warm_up/11-second_biggest.js
@@ -1,10 +1,12 @@
 #!/usr/bin/node
 let i = 2;
 let max = 0;
+let secondMax = 0;
 while (process.argv[i] !== undefined) {
   if (Number(process.argv[i]) > max) {
+    secondMax = max;
     max = Number(process.argv[i]);
   }
   i++;
 }
-console.log(max);
+console.log(secondMax);
