a427ca6a2764949a7dd3d73f172b0f6951abb85a
meddizzle316 <meddizzle316@gmail.com>
2024-03-25 13:25:16 -0500

    trying something out

--- javascript-warm_up/11-second_biggest.js
+++ javascript-warm_up/11-second_biggest.js
@@ -2,7 +2,7 @@
 let i = 2;
 let max = 0;
 let secondMax = 0;
-while (process.argv[i] !== undefined) {
+while (!isNaN(Number(process.argv[i]))) {
   if (Number(process.argv[i]) > max) {
     secondMax = max;
     max = Number(process.argv[i]);
