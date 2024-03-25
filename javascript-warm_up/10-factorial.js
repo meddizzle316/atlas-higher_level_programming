c597752ec0fdfec9464c0dd2ed89d780ab8c53e8
meddizzle316 <meddizzle316@gmail.com>
2024-03-25 12:55:11 -0500

    adding 10

---
+++ javascript-warm_up/10-factorial.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+function factorial (num) {
+  if (num === 1) {
+    return 1;
+  } else {
+    return num * (factorial(num - 1));
+  }
+}
+
+if (!isNaN(Number(process.argv[2]))) {
+  console.log(factorial(process.argv[2]));
+}
