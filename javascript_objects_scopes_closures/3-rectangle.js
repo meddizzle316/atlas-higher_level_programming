070918e267ad108d0e336a846a8d75d59d4e203c
meddizzle316 <meddizzle316@gmail.com>
2024-03-26 10:48:11 -0500

    first go at 3

---
+++ javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,22 @@
+#!/usr/bin/node
+class Rectangle {
+  constructor (w, h) {
+    if (w <= 0 || h <= 0 || !w || !h) {
+      /* */
+    } else {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      for (let x = 0; x < this.width; x++) {
+        process.stdout.write('X');
+      }
+      console.log('');
+    }
+  }
+}
+
+module.exports = Rectangle;
