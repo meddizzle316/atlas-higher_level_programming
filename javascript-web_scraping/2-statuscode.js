cbc36fcfe57dea9fecd1d7d4bde168201582e250
meddizzle316 <meddizzle316@gmail.com>
2024-03-28 12:36:13 -0500

    semi fix

--- javascript-web_scraping/2-statuscode.js
+++ javascript-web_scraping/2-statuscode.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 
-const request = require("request");
+const request = require('request');
 
 request(process.argv[2], function (err, response, body) {
   if (err) console.log(err);
-  console.log("code: " + response.statusCode);
+  console.log('code: ' + response.statusCode);
 });
