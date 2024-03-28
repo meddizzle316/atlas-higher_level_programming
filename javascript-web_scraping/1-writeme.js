0fa5e012b523346db1910e6ccf18b457651bcb0c
meddizzle316 <meddizzle316@gmail.com>
2024-03-28 12:26:04 -0500

    fixed fd accidental var

--- javascript-web_scraping/1-writeme.js
+++ javascript-web_scraping/1-writeme.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
 
-fd = require('fs');
+const fd = require('fs');
 const filePath = process.argv[2];
 const string = process.argv[3];
 fd.writeFile(filePath, string, {
   encoding: 'utf8'
 }, function (err, file) {
   if (err) throw err;
-})
+});
