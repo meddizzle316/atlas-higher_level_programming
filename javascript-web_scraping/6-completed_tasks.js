df64656b221bb7412b5c4379d1f6fd6c038bf8e9
meddizzle316 <meddizzle316@gmail.com>
2024-03-29 12:58:53 -0500

    not printing entries with values of 0

--- javascript-web_scraping/6-completed_tasks.js
+++ javascript-web_scraping/6-completed_tasks.js
@@ -5,19 +5,25 @@ let userList = [];
 request('https://jsonplaceholder.typicode.com/users', function (err, response, body) {
   if (err) console.log(err);
   userList = JSON.parse(body);
-  const endDict = {};
+  const tempDict = {};
   userList.forEach(user => {
-    endDict[user.id] = 0;
+    tempDict[user.id] = 0;
   });
   request(process.argv[2], function (err, response, body) {
     if (err) console.log(err);
     const list = JSON.parse(body);
     for (let i = 0; i < list.length; i++) {
       if (list[i].completed === true) {
-        const temp = endDict[list[i].userId];
-        endDict[list[i].userId] = temp + 1;
+        const temp = tempDict[list[i].userId];
+        tempDict[list[i].userId] = temp + 1;
       }
     }
+    const endDict = {};
+    Object.keys(tempDict).forEach(key => {
+      if (tempDict[key] !== 0) {
+        endDict[key] = tempDict[key];
+      }
+    })
     console.log(endDict);
   });
 });
